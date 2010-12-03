fdb03c99c848dcf55c12093e0a9d28f2ca103fa5
unknown <oi43839@.dip.banca.bnl.it>
2010-12-03 02:51:02 +0100

    Pushed a bit - allocating a 400mb buffer with 20000 objects for the test

--- src/org/directcache/test/BaseTest.java
+++ src/org/directcache/test/BaseTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.Vector;
 
 import org.directcache.CacheDescriptor;
@@ -19,25 +20,29 @@ public class BaseTest {
 	static DirectCache cache = null;
 	static int size = 0;
 	static int objects = 0;
-	static int objectsToStore = 500;
-	static int objectsSize = 100000;
-
-	@BeforeClass
-	public static void setup() {
-		cache = new DirectCache(1024*1024*50);
+	static int objectsToStore = 10000;
+	static int objectsSize = 20000;
+	static int mb2use = 400;
+	static Random generator = new Random();
+	
+	@Test
+	public void allocateMemory() {
+		cache = new DirectCache(1024*1024*mb2use);
+		System.out.println("allocated " + mb2use + " mb");
 	}
 	
 	@Test
 	public void storeObjects() throws Exception {
+		System.out.print("storing some objects... ");
 		for (int i = 0; i < objectsToStore; i++) {
-			TestClass p = new TestClass("test #" + i);
-			p.obj = new Object[objectsSize];
+			DummyObject p = new DummyObject("test #" + i);
+			p.obj = new Object[objectsSize*generator.nextInt(5)];
 			CacheDescriptor desc = cache.storeObject(p.getName(), p);
 			size += desc.getSize();
 			objects++;
 			//System.out.println("adding object of size " + desc.getSize());
 		}
-		System.out.println("stored some objects");
+		System.out.println("done");
 	}
 
 	@Test
@@ -51,7 +56,7 @@ public class BaseTest {
 		System.out.println("-----------------------------");
 
 		assertEquals (cache.getIndex().size(), objects);
-		assertEquals (cache.capacity(), 52428800);
+		assertEquals (cache.capacity(), mb2use*1024*1024);
 		assertEquals (cache.size(), size);
 		assertEquals (cache.remaining(), cache.capacity() - size);		
 	}
@@ -61,7 +66,7 @@ public class BaseTest {
 		Iterator<CacheDescriptor> index = cache.getIndex().values().iterator();
 		while (index.hasNext()) {
 			CacheDescriptor desc = index.next();
-			TestClass p = (TestClass)cache.retrieveObject(desc.getKey());
+			DummyObject p = (DummyObject)cache.retrieveObject(desc.getKey());
 			assertNotNull(p);
 			assertEquals(desc.getKey(), p.getName());
 		}		
@@ -92,9 +97,9 @@ public class BaseTest {
 	@Test
 	public void storeSomeObjectsAgain() throws Exception {
 		System.out.println("going to add again");
-		for (int i = 0; i < 500; i++) {
-			TestClass p = new TestClass("obj #" + i);
-			p.obj = new Object[objectsSize];
+		for (int i = 0; i < objectsToStore/2; i++) {
+			DummyObject p = new DummyObject("obj #" + i);
+			p.obj = new Object[objectsSize*generator.nextInt(5)];
 			CacheDescriptor desc = cache.storeObject(p.getName(), p);
 			assertNotNull(desc);
 			size += desc.getSize();
