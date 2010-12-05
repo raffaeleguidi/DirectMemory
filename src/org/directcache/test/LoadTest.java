0324b6a5b3fe87203bc01108825396650e7e825b
Raffaele P. Guidi <raffaele.p.guidi (at) gmail.com>
2010-12-05 19:11:04 +0100

    Some cleaning

--- src/org/directcache/test/LoadTest.java
+++ src/org/directcache/test/LoadTest.java
@@ -2,7 +2,6 @@ package org.directcache.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.util.Random;
 import java.util.logging.Logger;
