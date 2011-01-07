2e47ada289816dd127eb9a55a958beab4befb798
Raffaele P. Guidi <raffaele.p.guidi (at) gmail.com>
2011-01-07 13:21:09 +0100

    Separated basic from massive tests

--- DirectMemoryCache/src/test/java/org/directmemory/test/BasicTest.java
+++ DirectMemoryCache/src/test/java/org/directmemory/test/BasicTest.java
@@ -14,9 +14,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BasicSingleThreadedTest {
+public class BasicTest {
 
-	private static Logger logger=LoggerFactory.getLogger(BasicSingleThreadedTest.class);
+	private static Logger logger=LoggerFactory.getLogger(BasicTest.class);
 	
 	private Random random = new Random();
 
@@ -98,37 +98,7 @@ public class BasicSingleThreadedTest {
 		assertEquals(0, cache.onDiskEntriesCount());
 		assertEquals(0, cache.usedMemory());
 	}
-	
-	
-	@Test public void goOverTheOffheapLimitPutAndGet() {
-		int limit = 1000;
-		CacheStore cache = new CacheStore(limit, CacheStore.MB(2), 1);
-//		cache.serializer = new ProtoStuffSerializer();
-		for (int i = 1; i <= limit * 2; i++) {
-			DummyPojo pojo = new  DummyPojo("test" + i, randomSize());
-			cache.put("test" + i, pojo);
-			if (i <= limit) {
-				assertEquals(i, cache.heapEntriesCount());
-			} else {
-				assertEquals(limit, cache.heapEntriesCount());
-			}
-		}
-
-		logger.debug("goOverTheLimitPutAndGet " + cache.toString());
 		
-		for (int i = 1; i <= limit * 2; i++) {
-			String key = "test" + i;
-			DummyPojo newPojo = (DummyPojo)cache.get(key);
-			logger.debug("done " + i + " of " + (limit * 2));
-			assertNotNull(newPojo);
-			assertEquals("test"+i, newPojo.name);
-		}
-		logger.debug("finally " + cache.toString());
-		cache.reset();
-	}	
-	
-
-	
 	@Test
 	public void removeLast() {
 		CacheStore cache = new CacheStore(-1, 1 * 1024 * 1024, 1);
@@ -158,64 +128,6 @@ public class BasicSingleThreadedTest {
 		assertNull(entry);
 		cache.reset();
 	}
-	
-	@Test public void reachLimit() {
-		int limit = 10;
-		CacheStore cache = new CacheStore(limit, 1 * 1024 * 1024, 1);
-		
-		for (int i = 1; i <= limit; i++) {
-			cache.put("test" + i, new DummyPojo("test" + 1, 1024));
-			if (i < limit) {
-				assert(limit >= cache.heapEntriesCount());
-			}
-			logger.debug("reachLimit " + cache);
-		}
-		cache.reset();
-	}
-	
-	@Test public void goOverTheLimit() {
-		int limit = 10;
-		CacheStore cache = new CacheStore(limit, 1 * 1024 * 1024, 1);
-		for (int i = 1; i <= limit * 2; i++) {
-			DummyPojo pojo = new  DummyPojo("test" + i, 1024);
-			cache.put("test" + i, pojo);
-			if (i <= limit ) {
-				assertEquals(i, cache.heapEntriesCount());
-			} else {
-				assertEquals(limit, cache.heapEntriesCount());
-			}
-			logger.debug("goOverTheLimit " + cache);
-		}
-		cache.reset();		
-	}
-	
-	
-	@Test public void goOverTheLimitPutAndGet() {
-		int limit = 1000;
-		CacheStore cache = new CacheStore(limit, 10 * 1024 * 1024, 1);
-		for (int i = 1; i <= limit * 1.5; i++) {
-			DummyPojo pojo = new  DummyPojo("test" + i, 1024);
-			cache.put("test" + i, pojo);
-			if (i <= limit) {
-				assertEquals(i, cache.heapEntriesCount());
-			} else {
-				assertEquals(limit, cache.heapEntriesCount());
-			}
-		}
-
-		logger.debug("goOverTheLimitPutAndGet " + cache.toString());
-		
-		for (int i = 1; i <= limit * 1.5; i++) {
-			@SuppressWarnings("unused")
-			DummyPojo pojo = new  DummyPojo("test" + i, 1024);
-			DummyPojo newPojo = (DummyPojo)cache.get("test" + i);
-			assertNotNull(newPojo);
-			assertEquals("test"+i, newPojo.name);
-	}
-		assertEquals(limit, cache.heapEntriesCount());
-		cache.reset();
-	}	
-
 	
 	@AfterClass
 	public static void checkPerformance() {
