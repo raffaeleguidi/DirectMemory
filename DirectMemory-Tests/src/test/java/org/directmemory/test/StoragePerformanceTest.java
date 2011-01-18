6c3901a94fe48b766324dd25cd709c64f685d5dc
Raffaele P. Guidi <raffaele.p.guidi (at) gmail.com>
2011-01-18 09:33:40 +0100

    Small changes to speed tests

--- DirectMemory-Tests/src/test/java/org/directmemory/test/StoragePerformanceTest.java
+++ DirectMemory-Tests/src/test/java/org/directmemory/test/StoragePerformanceTest.java
@@ -10,9 +10,11 @@ import org.directmemory.cache.CacheEntry;
 import org.directmemory.cache.CacheManager;
 import org.directmemory.measures.Ram;
 import org.directmemory.misc.DummyPojo;
+import org.directmemory.serialization.OldProtoStuffSerializer;
 import org.directmemory.storage.FileStorage;
 import org.directmemory.storage.HeapStorage;
 import org.directmemory.storage.OffHeapStorage;
+import org.directmemory.storage.OrientDBBinaryStorage;
 import org.directmemory.storage.OrientDBStorage;
 import org.directmemory.storage.Storage;
 import org.junit.AfterClass;
@@ -32,29 +34,39 @@ public class StoragePerformanceTest {
 	private int pages = 1;
 	private int ramSize = (int)(payloadSize * howMany * 2.5) / pages ;
 
-	@Test
-	public void manyInsertsInHeap() {
+//	@Test
+	public void manyInsertsInHeap() throws InterruptedException {
 		repeatedInsert(new HeapStorage(), howMany);
 	}
 	
 //	@Test
-	public void manyInsertsOffHeap() {
+	public void manyInsertsOffHeap() throws InterruptedException {
 		logger.debug("Buffer size=" + Ram.inMb(ramSize*pages));
 		repeatedInsert(new OffHeapStorage(ramSize, pages), howMany);
 	}
 	
 //	@Test
-	public void manyInsertsOnDisk() {
+	public void manyInsertsOnDisk() throws InterruptedException {
 		repeatedInsert(new FileStorage(), howMany);
 	}
 	
-//	@Test
-	public void manyInsertsOnOrientDB() {
+	@Test
+	public void manyInsertsOnOrientDBBinary() throws InterruptedException {
+		repeatedInsert(new OrientDBBinaryStorage(), howMany);
+	}
+	
+	@Test
+	public void manyInsertsOnOrientDB() throws InterruptedException {
 		repeatedInsert(new OrientDBStorage(), howMany);
 	}
 	
-	public void repeatedInsert(Storage storage, int howMany) {		
+	public void repeatedInsert(Storage storage, int howMany) throws InterruptedException {		
 		logger.debug("starting with " + storage.getClass().toString());
+		storage.serializer = new OldProtoStuffSerializer();
+		Runtime.getRuntime().gc();
+		Thread.sleep(1000);
+		Runtime.getRuntime().gc();
+		logger.debug("Free memory: " + Runtime.getRuntime().freeMemory());	
 		logger.debug("before: " + storage.toString());
 		for (int i = 0; i < howMany; i++) {
 			CacheEntry entry = new CacheEntry();
@@ -72,6 +84,7 @@ public class StoragePerformanceTest {
 		logger.debug("read " + howMany + " entries");
 		logger.debug("after read: " + storage.toString());
 		logger.debug("overall performance: " + storage.performance());	
+		storage.dispose();
 	}
 	
 		
